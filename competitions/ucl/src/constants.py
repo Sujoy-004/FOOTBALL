eb37853bcd08797196c1f20b7eaad6d466b5c7e6
Sujoy <iamsujoy004@gmail.com>
2026-06-29 22:10:06 +0530

    feat(05-02): create UCL provider constants module
    - UCL_LEAGUE_ID=7, BSD_API_URL, CACHE_TTL_HOURS=1, CACHE_FILENAME
    - Module docstring on line 1
    - Follows worldcup constants pattern

---
+++ competitions/ucl/src/constants.py
@@ -0,0 +1,8 @@
+"""Constants for the UCL predictor.
+Extends football_core.constants with UCL-specific values.
+"""
+
+UCL_LEAGUE_ID: int = 7
+BSD_API_URL: str = "https://sports.bzzoiro.com/api/events/"
+CACHE_TTL_HOURS: int = 1
+CACHE_FILENAME: str = "cached_fixtures.json"
